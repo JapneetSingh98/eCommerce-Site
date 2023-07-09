cf77d4c22721cab092d7f17884c1dd45d7873912
Japneet Singh <japneetsingh@gmail.com>
2023-07-08 23:30:18 -0700

    conditional rendering and refactoring into methods

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,16 +4,76 @@ export default function MainContent(props) {
     const [contentDetails, setContentDetails] = useState({
         title: "Customers",
         customerCount: 5,
+        customers: [
+            {
+                key: 1,
+                id: 1,
+                name: "Scott",
+                phone: "123-456",
+                address: { city: "New York" },
+            },
+            {
+                key: 2,
+                id: 2,
+                name: "Smith",
+                phone: null,
+                address: { city: "London" },
+            },
+            {
+                key: 3,
+                id: 3,
+                name: "Kevin",
+                phone: "123-456",
+                address: { city: "San Francisco" },
+            },
+            {
+                key: 4,
+                id: 4,
+                name: "Linda",
+                phone: "123-456",
+                address: { city: "Paris" },
+            },
+            {
+                key: 5,
+                id: 5,
+                name: "Ben",
+                phone: null,
+                address: { city: "New York" },
+            },
+        ],
     });
-    
+
     function handleClick(event) {
         // const {name, value} = event.target;
         console.log("Refresh clicked");
         setContentDetails((prevState) => {
             return {
                 ...prevState,
-                "customerCount": contentDetails.customerCount + 1
-            }
+                customerCount: contentDetails.customerCount + 1,
+            };
+        });
+    }
+
+    function getPhoneToRender(phone) {
+        if (phone) {
+            return phone;
+        } else {
+            return <div className="bg-warning p-2">No Phone</div>;
+        }
+    }
+
+    function getCustomerRows() {
+        return contentDetails.customers.map((customer, i) => {
+            return (
+                <React.Fragment key={customer.key}>
+                    <tr>
+                        <th scope="row">{customer.id}</th>
+                        <td>{customer.name}</td>
+                        <td>{getPhoneToRender(customer.phone)}</td>
+                        <td>{customer.address.city}</td>
+                    </tr>
+                </React.Fragment>
+            );
         });
     }
 
@@ -31,6 +91,18 @@ export default function MainContent(props) {
                     Refresh
                 </button>
             </h4>
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">City</th>
+                    </tr>
+                </thead>
+                <tbody>{getCustomerRows()}</tbody>
+            </table>
         </React.Fragment>
     );
 }
